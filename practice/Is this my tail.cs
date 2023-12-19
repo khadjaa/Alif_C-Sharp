67ea4b237e658ae400c8b3c59d467fb382cbaa68
khadjaa <islomkhadja2001@icloud.com>
2023-12-19 12:00:00 +0500

    solved

---
+++ practice/Is this my tail.cs
@@ -0,0 +1,25 @@
+/*
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals 
+do not have the right tails. To help her, you must correct the broken function to make sure that 
+the second argument (tail), is the same as the last letter of the first argument (body) 
+- otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be non empty strings, and normal letters.
+*/
+
+using System;
+
+public class Kata
+{
+  public static bool CorrectTail(string body, string tail)
+  {
+    string sub = body.Substring(body.Length - tail.Length);
+
+    if (sub == tail)
+      return true;
+    else
+      return false;
+  }
+}
