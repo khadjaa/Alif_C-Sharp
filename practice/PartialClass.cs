38c15d67b7655ecf9177dd0b241d73c54714a548
khadjaa <islomkhadja2001@icloud.com>
2023-12-25 12:00:00 +0500

    solved

--- practice/PartialClass.cs
+++ practice/PartialClass.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
             {      
                 Console.WriteLine("Method1");
             }
+            partial void PartialMethod();
         }
         
         partial class MyClass
@@ -16,10 +17,43 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("Method2");
             }
+            
+            partial void PartialMethod()
+            {
+                Console.WriteLine("PartialMethod2");
+            }
+            
+            /*Partial methods полезны, когда вы хотите оставить возможность для генератора кода или
+             других инструментов вставить дополнительный код, и этот код может быть или 
+             не быть реализован в другом месте.*/
         }
+        
         /*Partial classes полезны при генерации кода, например, при использовании
-         средств визуального дизайна. Каждый файл содержит часть функциональности класса,
+         средств визуального дизайна. Каждый файл содержит часть функциональности класса, 
          и компилятор объединяет их в один класс.*/
+        
+        sealed class MySealedClass
+        {
+            // Sealed классы не могут быть унаследованы.
+            // Это полезно если мы не хотим чтобы другие классы наследовали функциональность нашего класса.
+        }
+        
+        public static class MyStaticClass
+        {
+            public static void StaticMethod1()
+            {
+                Console.WriteLine("StaticMethod1");
+            }
+
+            public static void StaticMethod2()
+            {
+                Console.WriteLine("StaticMethod2");
+            }
+        }
+        
+        /*Static классы предоставляют функциональность, которая не требует создания экземпляра объекта.
+         Static методы могут быть вызваны без создания объекта, что удобно для функций, 
+         не зависящих от состояния объекта.*/
         
         private static void Main(string[] args)
         {
